49ec1130e38eb99dd7846ffee2b6eb205847f778
Chris Young <chris@chrisyoung.org>
2013-12-02 16:06:08 +0000

    PAAS-1: Added @megastoat's joke

--- spec/PunsterSpec.js
+++ spec/PunsterSpec.js
@@ -1,3 +1,5 @@
+var punster = require('../src/punster');
+
 describe("punster", function() {
 
   it("should be able to make a pun", function() {
@@ -7,6 +9,17 @@ describe("punster", function() {
 
     pun = punster.pun(subject);
     expect(pun).toEqual(expected_pun);
+
+  });
+
+  it("should be able to make more than one pun", function() {
+
+    var puns = [{subject: "cock", pun: "The gun accidentally went off, now he's half cocked"},
+               {subject: "eggs", pun: "That's eggcellent"}];
+
+    for (var i = 0; i < puns.length; i++) {
+      expect(punster.pun(puns[i].subject)).toEqual(puns[i].pun);
+    }
 
   });
 
--- src/punster.js
+++ src/punster.js
@@ -1,10 +1,17 @@
 (function(exports){
 
     exports.pun = function(subject) {
-        if (subject != "eggs") {
-            throw "do not has";
+
+        var puns = [{subject: "cock", pun: "The gun accidentally went off, now he's half cocked"},
+                   {subject: "eggs", pun: "That's eggcellent"}];
+
+        for (var i = 0; i < puns.length; i++) {
+            if (puns[i].subject == subject) {
+                return(puns[i].pun);
+            }
         }
-        return "That's eggcellent";
+
+        throw "do not has";
     };
 
 })(typeof exports === 'undefined'? this['punster']={}: exports);

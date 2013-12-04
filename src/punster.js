f854122772f56ee31dbdf24f80faa069b8986994
Chris Young <chris@chrisyoung.org>
2013-12-04 22:14:52 +0000

    PAAS-1: Added Dene's joke

--- src/punster.js
+++ src/punster.js
@@ -11,7 +11,8 @@
         var puns = [{subject: "cock", pun: "The gun accidentally went off, now he's half cocked"},
                     {subject: "eggs", pun: "That's eggcellent"},
                     {subject: "art",  pun: "Mona Lisa walks up to Leonardo Da Vinci and says 'You got a match?' Yeah, your face and my art."},
-                    {subject: "plumbing", pun: "Fixing the toilet in the basement made me feel like I was plumbing new depths."}];
+                    {subject: "plumbing", pun: "Fixing the toilet in the basement made me feel like I was plumbing new depths."},
+                    {subject: "spam", pun: "The online supermarket keep sending me special offers on luncheon meat. I wish they'd stop spamming me."}];
 
         for (var i = 0; i < puns.length; i++) {
             if (puns[i].subject == subject) {
